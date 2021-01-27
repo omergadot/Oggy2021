c339315e7353b3f2a3d998ee4818b243d194feda
unknown <omer.gadot1@gmail.com>
2021-01-27 16:08:55 +0200

    added userhome component

---
+++ Client/src/components/mission.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+          <hr>
+
+    <v-layout row wrap justify-space-between>
+      <v-flex xs4 class="center">
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
+          height="40px"
+          contain
+        ></v-img>
+      </v-flex>
+      <v-flex xs3 >
+        
+      </v-flex>
+      <v-flex xs4 class="center">
+        <h3>תכשיר אותי</h3>
+        <h5>20 דק</h5>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
--- Client/src/pages/UserHome.vue
+++ Client/src/pages/UserHome.vue
@@ -1,14 +1,31 @@
 <template>
-  <div class="center">
-    <h1>היי, אני אוגי</h1>
+  <div>
+    <div class="center">
+      <h1>היי, אני אוגי</h1>
 
-    <h5>רוצה לשמור על הכשירות שלי?</h5>
-    <h5>בואו נסיים את ההכשרות בהקדם האפשרי</h5>
+      <h5>רוצה לשמור על הכשירות שלי?</h5>
+      <h5>בואו נסיים את ההכשרות בהקדם האפשרי</h5>
+    </div>
+    <br>
+    <div>
+      <h4 class="right">רוצה להציל את אוגי?</h4>
+      
+      <mission></mission>
+      <mission></mission>
+      <mission></mission>
+      <mission></mission>
+    </div>
   </div>
 </template>
 
 <script>
+import mission from "@/components/mission.vue";
+
 export default {
+  components: {
+    mission
+  },
+
   mixins: [],
 
   data() {
